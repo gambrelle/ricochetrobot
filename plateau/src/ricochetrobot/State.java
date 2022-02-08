82a0424a51dd07dfee2d1422626db0da02bd6484
gambrelle <34247906+gambrelle@users.noreply.github.com>
2022-02-08 15:07:26 +0100

    génération des obstacles double

--- plateau/src/ricochetrobot/State.java
+++ plateau/src/ricochetrobot/State.java
@@ -1,4 +1,8 @@
 package ricochetrobot;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.Random;
 public class State {
     protected int[][] board;
     protected int[][] posRobot;
@@ -30,6 +34,60 @@ public class State {
         this.board[9][7] = 11;
         this.board[8][6] = 12;
         this.board[7][6] = 12;
+
+        List<Integer> obstaclesDoubles = Arrays.asList(10, 11, 12, 13);// cf représentation des obstacles
+        List<Integer> obstaclesSimples = Arrays.asList(20, 21, 22, 23);
+        Random rand = new Random();
+        int compteurObstaclesSimples = 0;
+        int compteurObstaclesDoubles = 0;
+        while (compteurObstaclesSimples < 8){
+            int int_i = rand.nextInt(16);
+            int int_j = rand.nextInt(16);
+            if (this.board[int_i][int_j] == 0)
+            {
+                this.board[int_i][int_j] = obstaclesSimples.get(rand.nextInt(obstaclesSimples.size()));
+                compteurObstaclesSimples++;
+            }
+        }
+        while (compteurObstaclesDoubles < 16){
+            int int_i = rand.nextInt(15)+1;
+            int int_j = rand.nextInt(15)+1;
+            if (this.board[int_i][int_j] == 0)
+            {
+                this.board[int_i][int_j] = obstaclesDoubles.get(rand.nextInt(obstaclesDoubles.size()));
+                compteurObstaclesDoubles++;
+                //coin haut gauche
+                if (this.board[int_i][int_j]==10)
+                {
+                    this.board[int_i][int_j-1]=22;//obstacle droit sur case de gauche
+                    this.board[int_i-1][int_j]=23;//obstacle bas sur case au-dessus
+                }
+                //coin haut droit
+                if (this.board[int_i][int_j]==11)
+                {
+                    this.board[int_i][int_j+1]=22;//obstacle gauche sur case de droite
+                    this.board[int_i-1][int_j]=23;//obstacle bas sur case au-dessus
+                }
+                //coin bas droite
+                if (this.board[int_i][int_j]==12)
+                {
+                    this.board[int_i][int_j+1]=20;//obstacle gauche sur case de droite
+                    this.board[int_i+1][int_j]=21;//obstacle haut sur case au-dessous
+                }
+                //coin bas gauche
+                if (this.board[int_i][int_j]==11)
+                {
+                    this.board[int_i][int_j-1]=22;//obstacle droit sur case de gauche
+                    this.board[int_i+1][int_j]=21;//obstacle haut sur case au-dessous
+                }
+                
+            }
+        }
+        
+        //tirage des cases à modifier
+
+
+
 
     }
 }
