0a85e9acadecc8d0a4022ab3690acc356169e736
Rocc <74003169+RoccIrch@users.noreply.github.com>
2022-03-01 16:14:39 +0100

    suite

--- code/src/game/State.java
+++ code/src/game/State.java
@@ -6,11 +6,50 @@ public class State
 {
     int[][] board;
     int[][] posRobot;
+
     public State()
     {
-
+        this.board = {{0,0,0,0,10,12,0,0,0,0,0,10,12,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,13},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,13},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,10,12,0,0,0,0,0,0,0,0,0,0,0,0}};
+        this.board[7][7] = 24;
+        this.board[7][8] = 21;
+        this.board[8][8] = 22;
+        this.board[8][7] = 23;
+        this.board[6][7] = 13;
+        this.board[6][8] = 13;
+        this.board[7][9] = 14;
+        this.board[8][9] = 14;
+        this.board[9][8] = 11;
+        this.board[9][7] = 11;
+        this.board[8][6] = 12;
+        this.board[7][6] = 12;
     }
-    
+
+    public State getClone()
+    {
+        State s = new State();
+        for (int i = 0; i < s.board.length; i++)
+        {
+            for (int j = 0; j < s.board[i].length; j++)
+            {
+                s.board[i][j] = this.board[i][j];
+            }
+        }
+        for (int i = 0; i < s.posRobot.length; i++)
+        {
+            for (int j = 0; s.posRobot[i].length; j++)
+            {
+                s.posRobot[i][j] = this.posRobot[i][j];
+            }
+        }
+        return s;
+    }
+    public State play(Move move, int robot)
+    {
+        State s = this.getClone();
+        s.posRobot[robot][0] = move.getPosXF();
+        s.posRobot[robot][1] = move.getPosYF();
+        return s;
+    }
     public Move getRightMove(int robot)
     {
         for (int i = this.posRobot[robot][1]; i<16; i++)
