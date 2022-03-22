edbe20f84f8026008c05bcb4cb2011b8928959d1
RoccIrch <larocatonio@gmail.com>
2022-03-22 14:02:38 +0100

    changement de direction et intégration des collisions

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,12 +3,13 @@ package game;
 import gamegui.*;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class Main
 {
     public static void main(String[] args) throws IOException, InterruptedException {
-    	//RandomBoardGeneration generationAleatoire = new RandomBoardGeneration(); 	
+    	/*//RandomBoardGeneration generationAleatoire = new RandomBoardGeneration(); 	
     	//int[][] bo = generationAleatoire.getBoard();  	
         State s = new State();
         new Containers(s);
@@ -19,6 +20,15 @@ public class Main
             System.out.println(move.toString());    
         State s3 = s2.play(s.getUpMove(1), 1);
         for (Move move : s3.getMove(1))
-                System.out.println(move.toString());
+                System.out.println(move.toString());*/
+
+        State s = new State();
+        s.printBoard();
+        System.out.println(Arrays.deepToString(s.posRobot));
+        System.out.println(Arrays.deepToString(s.goalsToDo));
+
+        s = s.play(s.getUpMove(1), 1);
+
+        System.out.println(Arrays.deepToString(s.posRobot));
     }
 }
