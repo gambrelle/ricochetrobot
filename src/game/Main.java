957eb5cc7f2a294e860c25ec57fc4d7db950661d
Rocc <74003169+RoccIrch@users.noreply.github.com>
2022-03-28 14:12:23 +0200

    merge request

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,9 +1,11 @@
 package game;
 
 import gamegui.*;
+import solvers.*;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
 
 
 public class Main
@@ -28,13 +30,46 @@ public class Main
 
                 System.out.println(move.toString());*/
 
-        State s = new State();
+        /* State s = new State();
         s.printBoard();
         System.out.println(Arrays.deepToString(s.posRobot));
         System.out.println(Arrays.deepToString(s.goalsToDo));
 
+        for (int i = 0; i <= 3; i++)
+            for (Move move : s.getMove(i))
+                System.out.println(move.toString());
         s = s.play(s.getUpMove(1), 1);
+        
 
-        System.out.println(Arrays.deepToString(s.posRobot));
+        System.out.println(Arrays.deepToString(s.posRobot)); */
+
+        State s = new State();
+        System.out.println("Le plateau pour cette partie est : ");
+        s.printBoard();
+        System.out.println("Le position des robot pour cette partie est : " + Arrays.deepToString(s.posRobot));
+        System.out.println("Le position des objectifs pour cette partie est : " + Arrays.deepToString(s.goalsToDo));
+        s.chooseActivegoal(1);
+
+        Random r1 = new Random();
+        Random r2 = new Random();
+
+        while (!(s.isFinalState()))
+        {
+            //Test avec des mouvement aléatoire.
+            int n1 = r1.nextInt(4);
+            int n2 = 0;
+
+            while (s.getMove(n1).size() == 0)
+            {
+                n1 = r1.nextInt(4);
+            }
+
+            n2 = r2.nextInt(s.getMove(n1).size());
+            
+            System.out.println(n1 + " : " + (s.getMove(n1).get(n2)).toString());
+
+            s.play(s.getMove(n1).get(n2), n1);
+        }
+        
     }
 }
