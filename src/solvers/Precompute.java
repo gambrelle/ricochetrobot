d2204fb1c09ca4cf0ffbfdaa3777cfa9394a329f
gambrelle <34247906+gambrelle@users.noreply.github.com>
2022-04-03 19:38:14 +0200

    version sans if else

--- src/solvers/Precompute.java
+++ src/solvers/Precompute.java
@@ -20,7 +20,7 @@ public class Precompute{
         this.compteurCasesPleines = 0;
         this.etat = etat;
         this.jeton_objectif =jeton_objectif;
-        int limite = 16*16 - 5;
+        int limite = 16*16 - 7;
         int niveau = 0;
         for (int l = 0;l<16;l++){
             for(int r = 0; r <16;r++){
@@ -31,51 +31,10 @@ public class Precompute{
         int posToken[] =this.etat.getGoalsToDo()[this.etat.getActiveGoal()];
         this.mapPrecomputed[posToken[0]][posToken[1]]=-1;
         System.out.println("test 2");
-        while (compteurCasesPleines <= limite){
-            if (niveau == 0){
-                System.out.println("niveau 0");
-                ArrayList<Move> cache = this.etat.getMoveToken(jeton_objectif);
-                System.out.println("test avant move");
-                for (Move c : cache){
-                    System.out.println("test après move");
-                    System.out.println(c);
-                    if(c.getPosXF()!=c.getPosXI()){
-                        //deplacement latéral
-                        System.out.println("déplacement latéral");
-                        if(c.getPosXF()>c.getPosXI()){
-                            //déplacement vers la droite
-                            System.out.println("déplacement droite");
-                            for(int j= 1;j<(c.getPosXF()-c.getPosXI());j++){
-                                this.mapPrecomputed[c.getPosXI()+j][c.getPosYI()]=1;
-                            }
-                        }else{
-                            //déplacement vers la gauche
-                            System.out.println("déplacement gauche");
-                            for(int j= -1;j>(c.getPosXF()-c.getPosXI());j--){
-                                this.mapPrecomputed[c.getPosXI()+j][c.getPosYI()]=1;
-                            }
-                        }
-                    }else{
-                        //deplacement vertical
-                        System.out.println("déplacement vertical");
-                        if(c.getPosYF()>c.getPosYI()){
-                            //déplacement vers le bas
-                            for(int j= 1;j<(c.getPosYF()-c.getPosYI());j++){
-                                this.mapPrecomputed[c.getPosXI()][c.getPosYI()+j]=1;
-                            }
-                        }else{
-                            //déplacement vers le haut
-                            for(int j= -1;j>(c.getPosYF()-c.getPosYI());j--){
-                                this.mapPrecomputed[c.getPosXI()][c.getPosYI()+j]=1;
-                            }
-                        }
-                    }
-                }
-                niveau++;
-            }else{
+        while (niveau <= 8){
                 System.out.println("niveau "+niveau);
-               for (int l = 0;l<16;l++){
-                   for(int r = 0; r <16;r++){
+                for (int l = 0;l<16;l++){
+                    for(int r = 0; r <16;r++){
                         if (this.mapPrecomputed[l][r]==(niveau-1)){
                             ArrayList<Move> cache = this.etat.getMoveToken(jeton_objectif);
                             for (Move c : cache){
@@ -86,6 +45,7 @@ public class Precompute{
                                         for(int j= 1;j<(c.getPosXF()-c.getPosXI());j++){
                                             if (this.mapPrecomputed[c.getPosXI()+j][c.getPosYI()]==0){
                                                 this.mapPrecomputed[c.getPosXI()+j][c.getPosYI()]=niveau;
+                                                compteurCasesPleines++;
                                             };
                                         }
                                     }else{
@@ -93,6 +53,7 @@ public class Precompute{
                                         for(int j= -1;j>(c.getPosXF()-c.getPosXI());j--){
                                             if (this.mapPrecomputed[c.getPosXI()+j][c.getPosYI()]==0){
                                                 this.mapPrecomputed[c.getPosXI()+j][c.getPosYI()]=niveau;
+                                                compteurCasesPleines++;
                                             };
                                         }
                                     }
@@ -103,6 +64,7 @@ public class Precompute{
                                         for(int j= 1;j<(c.getPosYF()-c.getPosYI());j++){
                                             if(this.mapPrecomputed[c.getPosXI()][c.getPosYI()+j]==0){
                                                 this.mapPrecomputed[c.getPosXI()][c.getPosYI()+j]=niveau;
+                                                compteurCasesPleines++;
                                             }
                                         }
                                     }else{
@@ -110,15 +72,16 @@ public class Precompute{
                                         for(int j= -1;j>(c.getPosYF()-c.getPosYI());j--){
                                             if(this.mapPrecomputed[c.getPosXI()][c.getPosYI()+j]==0){
                                                 this.mapPrecomputed[c.getPosXI()][c.getPosYI()+j]=niveau;
+                                                compteurCasesPleines++;
                                             }
                                         }
                                     }
                                 }
                             }
                         }
-                   }
-               } 
-            }
+                    }
+                }
+                niveau++; 
         }
 
         
